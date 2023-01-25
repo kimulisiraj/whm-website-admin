d56d4e7e5bcf28f7a78855364e234292ed9830b7
Siraj Kimuli <pworld.designer@gmail.com>
2023-01-25 16:56:25 +0300

    adds sort to locations

--- app/Http/Controllers/Api/LocationController.php
+++ app/Http/Controllers/Api/LocationController.php
@@ -6,12 +6,26 @@ use App\Http\Controllers\Controller;
 use App\Models\Location;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class LocationController extends Controller
 {
     public function index(): Collection
     {
-        return Location::all();
+        return Location::query()
+            ->orderBy(
+                DB::raw('case 
+	                        when pastors_level= "Moment Leaders" then 1 
+	                        when pastors_level= "Network Leaders/Location Pastors" then 2 
+	                        when pastors_level= "Network Leader/Location Pastor" then 3 
+	                        when pastors_level= "Network Leader" then 4 
+	                        when pastors_level= "Cluster Leaders/Location Pastors" then 5 
+	                        when pastors_level= "Cluster Leader/Location Pastor" then 6 
+	                        when pastors_level= "Cluster Leader" then 7 
+	                        when pastors_level= "Location Pastors" then 8 
+	                        when pastors_level= "Location Pastor" then 9 
+                        end')
+                )->get();
     }
 
     public function show(Location $location): Location
