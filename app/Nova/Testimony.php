1681202188800063e309650eb9e7fd5878b996d0
Siraj Kimuli <pworld.designer@gmail.com>
2023-04-05 22:01:19 +0300

    adds author to testimony

--- app/Nova/Testimony.php
+++ app/Nova/Testimony.php
@@ -4,8 +4,6 @@ namespace App\Nova;
 
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\Avatar;
-use Laravel\Nova\Fields\ID;
-use Laravel\Nova\Fields\Markdown;
 use Laravel\Nova\Fields\Slug;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Trix;
@@ -14,36 +12,17 @@ use Laravel\Nova\Menu\MenuItem;
 
 class Testimony extends Resource
 {
-    /**
-     * The model the resource corresponds to.
-     *
-     * @var class-string<\App\Models\Testimony>
-     */
     public static $model = \App\Models\Testimony::class;
 
-    /**
-     * The single value that should be used to represent the resource when being displayed.
-     *
-     * @var string
-     */
     public static $title = 'title';
 
-    /**
-     * The columns that should be searched.
-     *
-     * @var array
-     */
     public static $search = [
         'title',
+        'author'
     ];
 
-    /**
-     * Get the fields displayed by the resource.
-     *
-     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
-     * @return array
-     */
-    public function fields(NovaRequest $request)
+
+    public function fields(NovaRequest $request): array
     {
         return [
             Avatar::make('Image', 'banner_image')
@@ -51,51 +30,28 @@ class Testimony extends Resource
                 ->prunable()
                 ->disk('public'),
             Slug::make('Slug')->from('Title')->hideFromIndex(),
-
             Text::make('Title')->rules('required', 'min:2', 'max:225')->required(),
-            Markdown::make('body')->rules('required')->required(),
+            Text::make('Author')->rules('required', 'min:2', 'max:225')->nullable(),
+            Trix::make('body')->rules('required')->required(),
         ];
     }
 
-    /**
-     * Get the cards available for the request.
-     *
-     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
-     * @return array
-     */
-    public function cards(NovaRequest $request)
+    public function cards(NovaRequest $request): array
     {
         return [];
     }
 
-    /**
-     * Get the filters available for the resource.
-     *
-     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
-     * @return array
-     */
+
     public function filters(NovaRequest $request)
     {
         return [];
     }
 
-    /**
-     * Get the lenses available for the resource.
-     *
-     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
-     * @return array
-     */
     public function lenses(NovaRequest $request)
     {
         return [];
     }
 
-    /**
-     * Get the actions available for the resource.
-     *
-     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
-     * @return array
-     */
     public function actions(NovaRequest $request)
     {
         return [];
