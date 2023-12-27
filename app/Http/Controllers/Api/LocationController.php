3b8a1588bc06681f220c2dae434e3b62d808baf5
Siraj Kimuli <pworld.designer@gmail.com>
2023-12-28 02:02:10 +0300

    Refactored LocationController with simplified sorting
    LocationController's sorting has been simplified by removing complex raw SQL queries and implementing a new function 'groupDataByPastorLevel'. This function utilizes the match and str_starts_with functions for better readability and ease of maintenance. The controller now groups data by pastor level, improves code clarity, and enhances performance efficiency.

--- app/Http/Controllers/Api/LocationController.php
+++ app/Http/Controllers/Api/LocationController.php
@@ -4,10 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Location;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Collection;
 
 class LocationController extends Controller
 {
@@ -20,27 +19,30 @@ class LocationController extends Controller
                     $query->where('location_name', 'LIKE', "%$request->search%")
                         ->orWhere('pastors_name', 'LIKE', "%$request->search%");
                 }
-            )
-            ->orderBy('sort')
-            ->orderBy(
-                DB::raw('case
-	                        when pastors_level= "Moment Leaders" then 1
-	                        when pastors_level= "Network Leaders/Location Pastors" then 2
-	                        when pastors_level= "Network Leader/Location Pastor" then 3
-	                        when pastors_level= "Network Leaders" then 4
-	                        when pastors_level= "Network Leader" then 5
-	                        when pastors_level= "Cluster Leaders/Location Pastors" then 6
-	                        when pastors_level= "Cluster Leader/Location Pastor" then 7
-	                        when pastors_level= "Cluster Leaders" then 8
-	                        when pastors_level= "Cluster Leader" then 9
-	                        when pastors_level= "Location Pastors" then 10
-	                        when pastors_level= "Location Pastor" then 11
-                        end')
-            )->get();
+            )->orderBy('sort')
+            ->get()
+            ->map([$this, 'groupDataByPastorLevel']);
+
     }
 
     public function show(Location $location): Location
     {
         return $location->load('activities');
+    }
+
+    public function groupDataByPastorLevel($item): array
+    {
+
+        $groupName = match (true) {
+            str_starts_with($item->pastors_level, 'Location Pastor') => 'locations',
+            str_starts_with($item->pastors_level, 'Cluster Leader') => 'clusters',
+            str_starts_with($item->pastors_level, 'Network Leader') => 'networks',
+            default => 'movement',
+        };
+
+        return [
+            ...$item->toArray(),
+            'group' => $groupName
+        ];
     }
 }
